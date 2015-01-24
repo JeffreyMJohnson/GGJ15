5653d4d3b7aeccb580ee5e4ee227be6d4795c3b6
Jeffrey Johnson <jefjohms@gmail.com>
2015-01-24 04:13:22 -0800

    initial commit. proto v1

---
+++ Assets/Player2Control.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof(Player2Control))]
+    public class Player2Control : MonoBehaviour
+    {
+        private PlatformerCharacter2D character;
+        private bool jump;
+        public bool isActive = false;
+
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        private void Update()
+        {
+            if (!jump)
+                // Read the jump input in Update so button presses aren't missed.
+                jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+
+        private void FixedUpdate()
+        {
+            if (isActive)
+            {
+                // Read the inputs.
+                bool crouch = Input.GetKey(KeyCode.LeftControl);
+                float h = CrossPlatformInputManager.GetAxis("Horizontal");
+                // Pass all parameters to the character control script.
+                character.Move(h, crouch, jump);
+                jump = false;
+            }
+        }
+
+        public void SwapPlayer()
+        {
+            isActive = !isActive;
+        }
+
+    }
+}
---
+++ Assets/PlayerSwapManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSwapManager : MonoBehaviour
+{	
+	// Update is called once per frame
+	void Update ()
+	{
+		// If the jump button is pressed and the player is grounded then the player should jump.
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			BroadcastMessage("SwapPlayer", null);
+		}
+	}
+}
