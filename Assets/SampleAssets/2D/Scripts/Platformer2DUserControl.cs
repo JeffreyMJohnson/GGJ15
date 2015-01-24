e80b76247279dbea81cdfb772fd6c23783990a61
Jeffrey Johnson <jefjohms@gmail.com>
2015-01-24 15:58:47 -0800

    proto complete.

--- Assets/SampleAssets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/SampleAssets/2D/Scripts/Platformer2DUserControl.cs
@@ -10,10 +10,13 @@ namespace UnitySampleAssets._2D
         private PlatformerCharacter2D character;
         private bool jump;
         public bool isActive = true;
+        private GameObject branchMantelPoint;
+
 
         private void Awake()
         {
             character = GetComponent<PlatformerCharacter2D>();
+            branchMantelPoint = GameObject.Find("branch/mantelPoint");
         }
 
         private void Update()
@@ -21,6 +24,14 @@ namespace UnitySampleAssets._2D
             if (!jump)
                 // Read the jump input in Update so button presses aren't missed.
                 jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            //if (isManteling)
+            //{
+            //    float distCovered = (Time.time - startTime) * speed;
+            //    float fracJourney = distCovered / journeyLength;
+            //    transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+            //    if (transform.position == endMarker.position)
+            //        isManteling = false;
+            //}
         }
 
         private void FixedUpdate()
@@ -33,7 +44,45 @@ namespace UnitySampleAssets._2D
                 // Pass all parameters to the character control script.
                 character.Move(h, crouch, jump);
                 jump = false;
+
             }
+
+        }
+
+        //test code for lerping up branch
+        //public Transform startMarker;
+        //public Transform endMarker;
+        //public float speed = 1.0f;
+        //private float startTime;
+        //private float journeyLength;
+        //public Transform target;
+        //public float smooth = 5.0f;
+        private bool isManteling;
+
+
+        void OnCollisionEnter2D(Collision2D collider)
+        {
+            Debug.Log(collider.transform.ToString());
+
+            if (collider.transform == branchMantelPoint.transform)
+            {
+                //startTime = Time.time;
+                //startMarker = transform.Find("GroundCheck");
+                //endMarker = GameObject.Find("branch").transform;
+                //BoxCollider2D endMarkerBox = endMarker.GetComponent<BoxCollider2D>();
+                //journeyLength = Vector3.Distance(startMarker.position, endMarker.position + endMarkerBox.bounds.extents);
+                Renderer rend = GetComponent<Renderer>();
+                transform.position = new Vector3(transform.position.x + rend.bounds.extents.x,
+                   rend.bounds.extents.y * .5f,
+                    transform.position.z);
+
+
+                ////playerBottomPos = branch.position;
+                //Vector3 sub = branchTopPos - playerBottomPos;
+                //transform.Translate(sub.x, sub.y, sub.z, null);
+                isManteling = true;
+            }
+
         }
 
         public void SwapPlayer()
